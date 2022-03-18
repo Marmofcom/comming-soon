84bb1bc7a6353ca9fbd1350394e32995a0021062
Manuk Minasyan <manuk.minasyan1@gmail.com>
2022-03-18 17:28:43 +0400

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { toggleDark } from '~/composables'
+
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
+
+<template>
+  <nav text-xl mt-6>
+    <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+      <div i="carbon-sun dark:carbon-moon" />
+    </button>
+  </nav>
+  <div>
+    © Copyright Minma. All rights reserved
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import {useUserStore} from '~/stores/user'
+
+const user = useUserStore()
+const name = ref(user.savedName)
+
+const router = useRouter()
+const notifyMe = () => {
+
+}
+
+const {t} = useI18n()
+</script>
+
+<template>
+  <div relative>
+    <div text-4xl mb-3 flex justify-center>
+      <img src="/logo.svg" dark:hidden class="w-32" alt="">
+      <img src="/logo-dark.svg" hidden dark:block class="w-32" alt="">
+    </div>
+    <h1 text-5xl dark:text-gray-400>{{ t('intro.desc') }} <span text-black dark:text-white>{{ t('intro.soon')}}</span></h1>
+    <p text-xl>
+      {{ t('intro.subscribe-and-get-notified') }}
+    </p>
+    <div my-10 flex items-center justify-center space-x-3>
+
+      <div>
+        <input
+            id="input"
+            v-model="name"
+            :placeholder="t('intro.whats-your-email-address')"
+            :aria-label="t('intro.whats-your-email-address')"
+            type="text"
+            autocomplete="false"
+            p="x4 y2"
+            w="250px"
+            text="center"
+            bg="transparent"
+            border="~ rounded gray-200 dark:gray-700"
+            outline="none active:none"
+            @keydown.enter="go"
+        >
+        <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
+
+      </div>
+      <button
+          btn
+          @click="notifyMe"
+      >
+        {{ t('button.notify-me') }}
+      </button>
+    </div>
+    <div flex justify-center>
+      <img src="/preview.png" z-20 rounded-md alt="">
+      <img src="/image-7.svg" absolute z-30 left-0 bottom-0 w-50 rounded-md class="-ml-35 -mb-10" alt="">
+      <img src="/image-8.svg" absolute right-0 w-30 z-10 rounded-md class="-mr-25 -mt-15" alt="">
+    </div>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+layout: home
+</route>
