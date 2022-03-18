18d51e3405cf2e4eebc02a3b9c79c30c3dea7707
Manuk Minasyan <manuk.minasyan1@gmail.com>
2022-03-18 18:08:47 +0400

    change icon color

--- unocss.config.ts
+++ unocss.config.ts
@@ -12,7 +12,7 @@ import {
 export default defineConfig({
   shortcuts: [
     ['btn', 'px-5 py-2 rounded inline-block bg-blue-600 text-white cursor-pointer hover:bg-blue-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-blue-600'],
   ],
   presets: [
     presetUno(),
