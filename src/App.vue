710bb477a508b91522c671f5b15c3049db0a0dd2
Manuk <manuk.minasyan1@gmail.com>
2022-04-09 18:27:07 +0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Minam',
+  title: 'The all-in-one management tool for your entire  product - Marmof',
   meta: [
     { name: 'description', content: 'Opinionated Vite Starter Template' },
   ],
